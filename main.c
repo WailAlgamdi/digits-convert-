94c6ffd574b8f67acc37b58b05df7cca26ff1292
WailAlgamdi <109033358+WailAlgamdi@users.noreply.github.com>
2023-11-21 18:55:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <strings.h>
 
 char* digit_to_letter(int digit) {
-    char* table[10] = {"ABC", "DEF", "GHI", "JKL", "MNO", "PRS", "TUV", "WXY"};
+    char* table[8] = {"ABC", "DEF", "GHI", "JKL", "MNO", "PRS", "TUV", "WXY"};
     
     if (digit >= 2) {
         return table[digit - 2]; 
@@ -21,7 +21,7 @@ int is_word_in_file(char* word, char* file) {
         return 0;
     }
 
-    char line[8];
+    char line[100];
     while (fgets(line, sizeof(line), fp) != NULL) {
         line[strcspn(line, "\n")] = 0;
         if (strcasecmp(word, line) == 0) {
@@ -35,8 +35,8 @@ int is_word_in_file(char* word, char* file) {
 }
 
 
-int number_to_word(char *word, int number, char* file) {
-  char *letters[7];
+int number_to_one_word(char *word, int number, char *file) {
+  char *letters[8];
   int index = 6;
   while (number > 0) {
     int digit = number % 10;
@@ -44,33 +44,31 @@ int number_to_word(char *word, int number, char* file) {
     number = number / 10;
     index--;
   }
-  int result = 0;
-  int i;
-  for (i = 0; i < 3; i++) {
-    word[0] = letters[0][i];
-    int j;
-    for (j = 0; j < 3; j++) {
-      word[1] = letters[1][j];
-      int k;
-      for (k = 0; k < 3; k++) {
-        word[2] = letters[2][k];
-        int l;
-        for (l = 0; l < 3; l++) {
-          word[3] = letters[3][l];
-          int m;
-          for (m = 0; m < 3; m++) {
-            word[4] = letters[4][m];
-            int n;
-            for (n = 0; n < 3; n++) {
-              word[5] = letters[5][n];
-              int o;
-              for (o = 0; o < 3; o++) {
-                word[6] = letters[6][o];
+  int i1;
+  for (i1 = 0; i1 < 3; i1++) {
+    word[0] = letters[0][i1];
+    int i2;
+    for (i2 = 0; i2 < 3; i2++) {
+      word[1] = letters[1][i2];
+      int i3;
+      for (i3 = 0; i3 < 3; i3++) {
+        word[2] = letters[2][i3];
+        int i4;
+        for (i4 = 0; i4 < 3; i4++) {
+          word[3] = letters[3][i4];
+          int i5;
+          for (i5 = 0; i5 < 3; i5++) {
+            word[4] = letters[4][i5];
+            int i6;
+            for (i6 = 0; i6 < 3; i6++) {
+              word[5] = letters[5][i6];
+              int i7;
+              for (i7 = 0; i7 < 3; i7++) {
+                word[6] = letters[6][i7];
                 word[7] = '\0';
-                result = is_word_in_file(word,file);
-                if (result == 1) {
+                if (is_word_in_file(word,file)) 
                   return 1;
-                }
+                
               }
             }
           }
@@ -82,29 +80,78 @@ int number_to_word(char *word, int number, char* file) {
 }
 
 
+int number_to_two_words_3_4(char *word,int number,char *file){
+  char *letters[8];
+  int index = 6;
+  while (number > 0) {
+    int digit = number % 10;
+    letters[index] = digit_to_letter(digit);
+    number = number / 10;
+    index--;
+  }
+  int i1;
+  for (i1 = 0; i1 < 3; i1++) {
+    word[0] = letters[0][i1];
+    int i2;
+    for (i2 = 0; i2 < 3; i2++) {
+      word[1] = letters[1][i2];
+      int i3;
+      for (i3 = 0; i3 < 3; i3++) {
+        word[2] = letters[2][i3];
+        word[3] = '\0';
+        if (is_word_in_file(word,file)) 
+          goto second_Word;
+          }
+        }
+      }
+      return 0;
+                
+   second_Word:
+   char second_word[5];
+   word[3] = '-';
+    int i4;
+    for (i4 = 0; i4 < 3; i4++) {
+      word[4] = letters[4][i4];
+      int i5;
+      for (i5 = 0; i5 < 3; i5++) {
+        word[5] = letters[5][i5];
+        int i6;
+        for (i6 = 0; i6 < 3; i6++) {
+          word[6] = letters[6][i6];
+          int i7;
+          for (i7 = 0; i7 < 3; i7++) {
+            word[7] = letters[7][i7];
+            word[8] = '\0';
+            strcpy(second_word, word + 4);
+            if (is_word_in_file(second_word,file)) 
+              return 1;
+           }
+         }
+       }
+     }
+   
+   
+   return 0;
+           
 
-int main() {
-    char word[8];
-    int number;
-    printf("Enter a six-digit number: "); 
-    scanf("%d", &number);
-    number_to_word(word,number, "words.txt");
-    
-    if (word != NULL) {
-    printf("The word is: %s\n", word);
-} else if (strlen(word) > 0) {
-    printf("The word is: %s\n", word);
-} else {
-    printf("No word found for the number.\n");
 }
 
 
+int main(){
+    char word[9];
+    char file[] = "words.txt";
+    int number;
+    printf("Enter a Seven-digit number: "); 
+    scanf("%d", &number);
+  int result = number_to_two_words_3_4(word,number, file);
+    
+    if (result) 
+    printf("The word is: %s\n", word);
+    else 
+    printf("No word found for the number.\n");
 
-    if (strlen(word) > 0) {
-        printf("The word is: %s\n", word);
-    } else {
-        printf("No word found for the number.\n");
-    }
+
+
 
     return 0;
 }
